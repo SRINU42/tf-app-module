dc6622b79e96d989c6bf03f3f96a7c5527e5e5ad
SRINU <vasu_anand01@yahoo.co.in>
2024-01-08 08:59:39 +0530

    tf

--- main.tf
+++ main.tf
@@ -1,10 +1,3 @@
-#resource "null_resource" "test" {
-#    provisioner "local-exec" {
-#        command = "echo HelloWorld - ENV - ${var.env}"
-#
-#    }
-#}
-
 resource "null_resource" "test" {
 
     provisioner "local-exec" {
